92fa216f7c008ca1789f52513c0a0688f7238c0f
Jison-James <jisonjames11@gmail.com>
2026-07-09 23:54:34 +0530

    Fix password hashing in Django Admin for custom User model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Info', {'fields': ('role', 'employee_id', 'full_name')}),
+    )
+    list_display = ['email', 'username', 'role', 'is_staff']
+
+admin.site.register(User, CustomUserAdmin)
 
